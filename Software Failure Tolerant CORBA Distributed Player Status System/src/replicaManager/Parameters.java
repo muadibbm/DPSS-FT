d6fc94cf86fb57a13684eea9bf315880b76b5b7d
desydesign <desy.andreeva@yahoo.com>
2014-04-12 19:00:35 -0400

    Replica Manager

--- Software Failure Tolerant CORBA Distributed Player Status System/src/replicaManager/Parameters.java
+++ Software Failure Tolerant CORBA Distributed Player Status System/src/replicaManager/Parameters.java
@@ -10,12 +10,46 @@ public class Parameters
 	
 	protected static int UDP_PORT_REPLICA_MANAGER = 5000;
 	
+	protected static int UDP_PORT_FE = 6000;
+	
 	protected static enum METHOD_CODE 
 	{
-	    START_REPLICA, STOP_REPLICA;
+	    CREATE_ACCOUNT
+	    , PLAYER_SIGN_IN
+	    , PLAYER_SIGN_OUT
+	    , TRANSFER_ACCOUNT
+	    , GET_PLAYER_STATUS
+	    , SUSPEND_ACCOUNT
+	    , START_REPLICA
+	    , STOP_REPLICA;
 	}
+	
+	protected static String UDP_PARSER = "/";
 	
 	protected static int UDP_BUFFER_SIZE = 10000;
 	
-	protected static String UDP_PARSER = "/";
+	protected static String FE_NAME = "FE";
+	protected static String RM_NAME = "RM"; 
+	protected static String RA_NAME = "RA";
+	protected static String RB_NAME = "RB";
+	protected static String LR_NAME = "LR";
+	
+	protected static String GeoLocationOfGameServerNA = "132";
+	protected static String GeoLocationOfGameServerEU = "93";
+	protected static String GeoLocationOfGameServerAS = "182";
+	
+	protected static final int NA_IP_ADDRESS = 132;
+	protected static final int EU_IP_ADDRESS = 93;
+	protected static final int AS_IP_ADDRESS = 182;
+	
+	
+	protected static int UDP_PORT_REPLICA_B_NA = 3001;
+	protected static int UDP_PORT_REPLICA_B_EU = 3100;
+	protected static int UDP_PORT_REPLICA_B_AS = 3200;
+	
+	protected static String SERVER_LOG_EXTENSION = "_REPLICA_B_LOG.txt";
+	
+	protected static String NA_SERVER_ACRO = "NA";
+	protected static String EU_SERVER_ACRO = "EU";
+	protected static String AS_SERVER_ACRO = "AS";
 }
--- Software Failure Tolerant CORBA Distributed Player Status System/src/replicaManager/ReplicaManager.java
+++ Software Failure Tolerant CORBA Distributed Player Status System/src/replicaManager/ReplicaManager.java
@@ -1,9 +1,69 @@
 package replicaManager;
 
+import java.io.*;
+import java.net.*;
+
 public class ReplicaManager 
 {
+	
+	
+	
+	static DatagramSocket aSocket = null;
+	static boolean waitForConnection = true;
+	static String serverIPAddress;
+	static int IPaddress = 0;
+	static int serverPort;
+	static String dataRecieved = null;
+	static String [] messageArray;
+	static int parserPosition = 0;
+	
+	
 	public ReplicaManager()
 	{
+		
+	}
+	
+	public static void main (String [] args) {
+		
+		startServer(8888);
+		
+	}
+	
+	public static void startServer (int portNumber) {
+		String requestServerInitials = null;
+		String requestMethodCode = null;
+	
+		try {
+			
+			aSocket = new DatagramSocket(Parameters.UDP_PORT_REPLICA_B);
+			byte [] buffer = new byte [Parameters.UDP_BUFFER_SIZE];
+			
+			//always listen for new messages on the specified port
+			while (waitForConnection) {							
+				
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				aSocket.receive(request);
+
+				//get the data from the request and check
+				dataRecieved = new String(request.getData());
+				//dataRecieved.toUpperCase();
+				messageArray = dataRecieved.split(Parameters.UDP_PARSER);
+				
+				parserPosition = dataRecieved.indexOf(Parameters.UDP_PARSER);
+				System.out.println(dataRecieved);
+				
+				//get who is initiating the request : RM, Leader, Replica1 or Replica2
+				requestServerInitials = messageArray[0];
+				requestMethodCode = messageArray[1]; 
+			}
+		}
+			catch (Exception e) {e.printStackTrace();} 
+		}
+		
+	
+	
+	public static void stopServer (int portNumber) {
+		
 		
 	}
 }
