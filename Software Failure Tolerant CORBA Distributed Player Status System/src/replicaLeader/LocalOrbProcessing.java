cfda0cc2fcd2d8c650677fcae1570b7d237b7a1a
Vipul Kapoor <Vipul Kapoor@VipulKapoor-PC.wireless.concordia.ca>
2014-04-12 11:49:25 -0400

    Updates from LeaderServer

---
+++ Software Failure Tolerant CORBA Distributed Player Status System/src/replicaLeader/LocalOrbProcessing.java
@@ -0,0 +1,28 @@
+package replicaLeader;
+
+public class LocalOrbProcessing 
+{
+	// extract required parameters from the input data
+	private String[] extractParameters()
+	{
+		return null;
+	}
+	
+	// extract ip address, get required Local Server <location> reference
+	private GameServerInterface  getServerReference()
+	{
+		return null;
+	}
+	
+	protected void performRMI(String p_input)
+	{
+		// getServerReference
+		// extract function name
+			// depen
+		
+	}
+	
+	// send CORBA method invocation to Local Server <location>
+	
+	// Disconnect when required - never, for now
+}
