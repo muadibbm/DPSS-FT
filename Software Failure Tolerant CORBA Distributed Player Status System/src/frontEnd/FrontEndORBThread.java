3450101ac351d88dc1f111aecdc1349592e964f1
Mehrdad <muadibbm@yahoo.com>
2014-04-12 23:12:12 -0400

    FE fully implemented. it works!

--- Software Failure Tolerant CORBA Distributed Player Status System/src/frontEnd/FrontEndORBThread.java
+++ Software Failure Tolerant CORBA Distributed Player Status System/src/frontEnd/FrontEndORBThread.java
@@ -9,12 +9,39 @@ import org.omg.CORBA.ORB;
 class FrontEndORBThread extends Thread
 {
 	private ORB orb;
+	private static boolean bLeaderResponded;
+	private static boolean bConfirmation;
 
 	protected FrontEndORBThread(ORB pOrb)
 	{
 		orb = pOrb;
+		bLeaderResponded = false;
+		bConfirmation = false;
 	}
 
+	protected static void setLeaderResponded(boolean pResponded)
+	{
+		bLeaderResponded = pResponded;
+	}
+	
+	// Used to check if the leader has sent back the result
+	protected static boolean hasLeaderResponded()
+	{
+		return bLeaderResponded;
+	}
+	
+	protected static void setConfimation(boolean pConfirmation)
+	{
+		bConfirmation = pConfirmation;
+	}
+	
+	// Used to check if the method invoked was a success or failure
+	protected static boolean getConfirmation()
+	{
+		return bConfirmation;
+	}
+	
+	
 	public void run()
 	{
 		orb.run();
