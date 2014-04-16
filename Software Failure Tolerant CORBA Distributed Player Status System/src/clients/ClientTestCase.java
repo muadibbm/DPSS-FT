76da4c53b0578aa18714d33c719146409245783b
desydesign <desy.andreeva@yahoo.com>
2014-04-15 21:01:54 -0400

    fix

--- Software Failure Tolerant CORBA Distributed Player Status System/src/clients/ClientTestCase.java
+++ Software Failure Tolerant CORBA Distributed Player Status System/src/clients/ClientTestCase.java
@@ -12,7 +12,7 @@ class ClientTestCase
 		//admin.getPlayerStatus("Admin", "Admin", "132.20.20.20");
 		//player.playerSignOut("user123", "132.10.10.10");
 		//admin.getPlayerStatus("Admin", "Admin", "132.20.20.20");
-		player.transferAccount("user123", "123456", "132.10.10.10", "93.10.10.10");
+		//player.transferAccount("user123", "123456", "132.10.10.10", "93.10.10.10");
 		//admin.getPlayerStatus("Admin", "Admin", "93.20.20.20");
 		//admin.suspendAccount("Admin", "Admin", "93.20.20.20", "user123");
 		//admin.getPlayerStatus("Admin", "Admin", "93.20.20.20");
